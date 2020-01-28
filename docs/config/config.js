24b6ffc248535f91fd6372c86276a0df0978ccda
hero2bash <zero.bash@gmail.com>
2020-01-28 14:44:34 +0800

    Add interaction sample

--- docs/config/config.js
+++ docs/config/config.js
@@ -2,8 +2,8 @@ export default {
     clientIDs: {        
         'mypurecloud.com': 'e7de8a75-62bb-43eb-9063-38509f8c21af'
     },
-    "redirectUriBase": "https://princemerluza.github.io/purecloud-partner-listing-management/",
-    //"redirectUriBase": "http://localhost:8080/",
+    //"redirectUriBase": "https://princemerluza.github.io/purecloud-partner-listing-management/",
+    "redirectUriBase": "http://localhost:8080/",
 
     // PureCloud assigned name for the premium app
     // This should match the integration type name of the Premium App
